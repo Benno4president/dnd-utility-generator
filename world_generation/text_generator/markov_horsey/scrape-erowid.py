60194e2d0647c25720f9daf53391252f6f9b4b9f
Benno4president <krijoh.b@gmail.com>
2022-03-27 23:25:44 +0200

    fixed markov horsey scraper csv + author

--- world_generation/text_generator/markov_horsey/scrape-erowid.py
+++ world_generation/text_generator/markov_horsey/scrape-erowid.py
@@ -21,7 +21,7 @@ def removeHTML(s):
     return s
 
 baseurl = 'http://www.erowid.org/experiences/exp.php?ID=%s'
-maxID = 2 # 67000
+maxID = 3 # 67000
 dir = 'text/'
 if not os.path.exists(dir):
     os.mkdir(dir)
@@ -32,13 +32,13 @@ if not os.path.exists(dir):
 
 print('---------------------------------------------------------------------------------\\')
 print('Downloading Erowid experience reports')
-print('Hit control-C to quit')
-story_dict = []
+story_list = []
 for i in range(1, maxID):
+    fn = dir + 'erowid_' + str(i) + '.txt'
     id = i#random.randint(1,maxID)
     url = baseurl % id
 
-    if os.path.exists(dir + 'erowid_' + str(i) + '.txt'):
+    if os.path.exists(fn):
         print('We already downloaded that one.  Skipping #'+str(i))
         continue
 
@@ -52,29 +52,20 @@ for i in range(1, maxID):
         continue
 
     # sleeping to not get ip banned for spam, after continue calls.
-    time.sleep(1)
+    time.sleep(0.8)
     
-    # find the list of substances
-    dosechart = page.split('DoseChart')[1]
-    lines = dosechart.splitlines()
-    substances = []
-    for line in lines:
-        if '<td><a ' in line:
-            substance = line.split("'>")[1].split('<')[0]
-            substanceUrl = line.split("href='")[1].split("'")[0]
-            substanceUrl = substanceUrl.split('/')[-2]
-            substances.append(substanceUrl)
-    substances = sorted(list(set(substances)))
-    fn = dir + 'erowid_' + str(i) + '.txt'
-    title = page.split('class="title">')[1].split('</div>')[0]
-
-    print('Substances: %s'%substances)
-    print('Filename: %s'%fn)
+    title = page.split('class="title">')[1].split('</div>')[0].replace('\n', '')
+    substance = page.split('class="substance">')[1].split('</div>')[0].replace('\n', '')
+    author = page.split('class="author">')[1].split('</a>')[0].split('>')[1].replace('\n', '')
+    
     print('Title: %s'%title)
+    print('Substance: %s'%substance)
+    print('Author: %s'%author)
 
     # get main text and remove common unicode characters
     body = page.split('Start Body -->')[1].split('<!-- End Body')[0]
     body = body.replace('\r','')
+    body = body.replace('\n','')
     body = body.replace('\x92',"'")
     body = body.replace('\x93','"')
     body = body.replace('\x94','"')
@@ -82,14 +73,16 @@ for i in range(1, maxID):
     body = removeHTML(body)
     body = body.strip()
 
-    story_dict.append({'title':title, 'story': body})
+    story_list.append({'title':title, 'author':author, 'story': body})
 
     # uncomment to save txt file.
     #writeFile(fn, title + '\n\n' + body + '\n')
+
+# This converts and saves the data as csv
 if True:
-    with open('dict.csv', 'w') as csv_file:  
+    with open(f'scraped_{len(story_list)}.csv', 'w') as csv_file:  
         writer = csv.writer(csv_file)
-        for key, value in story_dict:
-            print(key, value)
-            writer.writerow([value[0], value[1]])
+        writer.writerow(list(story_list[0].keys()))
+        for story_page in story_list:
+            writer.writerow([story_page['title'], story_page['author'], story_page['story']])
     
